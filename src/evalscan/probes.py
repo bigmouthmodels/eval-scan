351cd89a46846060fa9220065809c88ef9f0ecd9
Jerome.Wynne <Jerome.Wynne@dsit.gov.uk>
2025-05-13 08:38:55 +0000

    Include precommit hooks.

--- src/evalscan/probes.py
+++ src/evalscan/probes.py
@@ -1,12 +1,15 @@
 import asyncio
 from datetime import datetime, timezone
+from math import log
 from uuid import uuid4
 
 import duckdb
+from inspect_ai.model import GenerateConfig, get_model
 import pandas as pd
 from tqdm.asyncio import tqdm_asyncio
 
-from faker import Faker    
+from faker import Faker
+
 fake = Faker()
 
 
@@ -22,7 +25,7 @@ def apply_async(df, async_fn) -> pd.Series:
 def dummy_probe(df: pd.DataFrame, con: duckdb.DuckDBPyConnection):
     # df is tidy messages df
     score_name = "dummy_scorer"
-    
+
     async def dummy_scorer(row: pd.Series):
         await asyncio.sleep(0.01)
         uuid = uuid4()
@@ -32,24 +35,26 @@ def dummy_probe(df: pd.DataFrame, con: duckdb.DuckDBPyConnection):
         input_tokens = 0
         output_tokens = 0
         metadata = ""
-        con.execute("INSERT INTO message_scores VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [uuid, message_uuid, inserted, score_name, score, input_tokens, output_tokens, metadata])
-        
-    existing_scores = con.execute("SELECT * FROM message_scores WHERE score_name = (?)", [score_name]).df()
-    apply_async(df[~df["uuid"].isin(existing_scores["message_uuid"].unique())], dummy_scorer)
+        con.execute(
+            "INSERT INTO message_scores VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            [
+                uuid,
+                message_uuid,
+                inserted,
+                score_name,
+                score,
+                input_tokens,
+                output_tokens,
+                metadata,
+            ],
+        )
 
-
-
-
-
-async def awake_scorer(row: pd.Series) -> str:  # Test scorer
-    await asyncio.sleep(0.01)
-    if uniform(0.0, 1.0) > 0.5:
-        return "Awake!"
-    else:
-        return "Asleep..."
-
-
-from inspect_ai.model import GenerateConfig, get_model
+    existing_scores = con.execute(
+        "SELECT * FROM message_scores WHERE score_name = (?)", [score_name]
+    ).df()
+    apply_async(
+        df[~df["uuid"].isin(existing_scores["message_uuid"].unique())], dummy_scorer
+    )
 
 
 def is_reasoning_message(row: pd.Series):

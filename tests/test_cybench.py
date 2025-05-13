1a990e3136c2989c1ed8b38e82c6ec809b2d49a5
Jerome.Wynne <Jerome.Wynne@dsit.gov.uk>
2025-05-13 09:53:24 +0000

    Update maxfilesize in precommit hooks and include gdm eval log.

--- tests/test_cybench.py
+++ tests/test_cybench.py
@@ -1,28 +1,30 @@
 from pathlib import Path
 
 
-from evalscan.cli import index, scan, report
+from evalscan.index import index
+from evalscan.scan import scan
+from evalscan.report import report
 
 
 def test_index(tmp_path):
     index(
-        logs_dir=Path(__file__).parent / "assets" / "cybench-100",
-        db_uri=tmp_path / "cybench-100.db",
+        logs_dir=str(Path(__file__).parent / "assets" / "cybench-100"),
+        db_uri=str(tmp_path / "cybench-100.db"),
     )
 
 
 def test_scan(tmp_path):
     index(
-        logs_dir=Path(__file__).parent / "assets" / "cybench-100",
-        db_uri=tmp_path / "cybench-100.db",
+        logs_dir=str(Path(__file__).parent / "assets" / "cybench-100"),
+        db_uri=str(tmp_path / "cybench-100.db"),
     )
-    scan(db_uri=tmp_path / "cybench-100.db")
+    scan(db_uri=str(tmp_path / "cybench-100.db"))
 
 
 def test_report(tmp_path):
     index(
-        logs_dir=Path(__file__).parent / "assets" / "cybench-100",
-        db_uri=tmp_path / "cybench-100.db",
+        logs_dir=str(Path(__file__).parent / "assets" / "cybench-100"),
+        db_uri=str(tmp_path / "cybench-100.db"),
     )
-    scan(db_uri=tmp_path / "cybench-100.db")
-    report(db_uri=tmp_path / "cybench-100.db")
+    scan(db_uri=str(tmp_path / "cybench-100.db"))
+    report(db_uri=str(tmp_path / "cybench-100.db"))
